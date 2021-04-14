2d2a1de542307e9308af392e2a802ed81d1f9dd3
aishwaryasurwase <31077384+aishwaryasurwase@users.noreply.github.com>
2021-04-14 10:08:43 +0530

    Delete branch

--- src/components/Greet/Greet.js
+++
@@ -1,7 +0,0 @@
-// const greet = (props) => <h1>Welcome {props.name}</h1> 
-// export default greet;
-export const Greet = (props) => {
-    return (
-        <div><p>{props.name} want to go {props.location}</p>
-            <p>{props.children}</p></div>)
-}
--- src/components/Welcome/Welcome.js
+++
@@ -1,8 +0,0 @@
-import React, {Component} from 'react';
-class Welcome extends Component{
-    render(){
-        return <p>Welcome {this.props.name} to {this.props.location}</p>
-        // return React.createElement('div', {id: 'aish'}, React.createElement('h1', null, 'crazy girls'))
-    }
-}
-export default Welcome
